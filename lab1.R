9899afd2ae80d79bab23d204ea464d26c11336c9
gregorgrbec <ggrbec@gmail.com>
2018-10-12 12:44:36 +0200

    Update lab1.R
    very hard lab

--- lab1.R
+++ lab1.R
@@ -1,1 +1,29 @@
 print("Živjo!")
+
+n<-c(1:100)
+l<-list(1:10)
+m<-matrix(nrow=10, ncol=5, byrow=T)
+
+
+ll<-list("a","b","c","d")
+
+
+a<-"a"
+b="b"
+c<-"c"
+d<-"d"
+
+
+d<-data.frame(x,y,z,d)
+colnames(d) = c("Stolpec1","Stolpec2","Stolpec3", "Stolpec4")
+
+
+
+
+#Exercise1
+
+x<-c(1:3)
+y<-c(4:6)
+z<-(7:9)
+
+vecmatrix <- matrix(nrow=3, ncol=3)
